1b61b40cd04184492e34be90574876db659920ef
Turki AL Mutairi <shalahe30@gmail.com>
2024-03-28 00:34:05 +0300

    1st

---
+++ sub/index.js
@@ -0,0 +1,20 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('sub', (req, res) => {
+    if (req.method === 'POST') {
+      try {
+        const data = req.body;
+        if (!data || !data.X || !data.Y) {
+          res.status(400).send('Invalid JSON format or missing X/Y values.');
+          return;
+        }
+
+        data.Result =  parseInt(data.X) - parseInt(data.Y) ;
+        res.status(200).json(data);
+      } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+  } else {
+    res.status(405).send('GET method Not Allowed');
+  }
+});
